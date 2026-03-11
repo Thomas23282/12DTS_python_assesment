77aa178b684b40c6ad6489cc20825fcc65fd752c
Thomas23282 <23282@wc.school.nz>
2026-03-12 12:07:53 +1300

    Second commit, mainly for testing. Added name check and slow typing

--- Thomas_Affleck_Python_assesment.py
+++ Thomas_Affleck_Python_assesment.py
@@ -5,19 +5,30 @@ import time
 # Global variables
 own_characters = []
 #enemy_characters = [{"Name" : }]
+TEXT_SLEEP_TIME = 0.1
 
 #Functions
+def slow_type(sentence):
+    for i in range(len(sentence)):
+        print(sentence[i], end = "")
+        time.sleep(TEXT_SLEEP_TIME)
+    print()
+
 def main_menu(): #Menu to run at start
-    print("You are stuck in space")
+    slow_type("You are stuck in space")
     time.sleep(0.5)
-    print("You will have to make your way back to Earth while completing challenges along the way.")
+    slow_type("You will have to make your way back to Earth while completing challenges along the way.")
     time.sleep(0.5)
+
 
 def name_check():
     global name
     name = input("What is your name? ")
     print(f"Your name is {name}")
 
+
+
+
 #Main
-#main_menu()
-name_check()
+main_menu()
+#name_check()
