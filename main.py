2245aeb42a3186aa694c2bd618d28cd615637fed
Thomas23282 <23282@wc.school.nz>
2026-03-12 11:50:30 +1300

    Initial commit, basic intro

---
+++ main.py
@@ -0,0 +1,18 @@
+# Libraries
+import random
+import time
+
+# Global variables
+own_characters = []
+#enemy_characters = [{"Name" : }]
+
+#Functions
+def main_menu(): #Menu to run at start
+    print("You are stuck in space")
+    time.sleep(0.5)
+    print("You will have to make your way back to Earth while completing challenges along the way.")
+    time.sleep(0.5)
+
+
+#Main
+main_menu()
